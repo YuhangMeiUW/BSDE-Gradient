6633a01a21f5a813f7821b350b795d1baa4a13cd
Yuhang Mei <yuhangmei@Yuhangs-Mac-Studio.local>
2026-02-28 19:45:55 -0800

    update

--- finetune_diffusion.py
+++ finetune_diffusion.py
@@ -14,17 +14,18 @@ N = 10000 # Number of training samples
 dt = 0.02  # Time step size
 steps = int(T/dt)  # Number of time steps
 noise_level = 2  # Noise level in the SDE
-kf = 50 # iterations for whole procedure
-opt_iter = 10
+kf = 100 # iterations for whole procedure
+opt_iter = 3
 phi_iter = 10
 # temperature = 50.0
 # u_iter = 10000
 # Temperature schedule
-temperature_schedule = lambda k: 50.0 * (1 / 50)**(k/39) if k< 40 else 1.0  # Exponential decay schedule for temperature
+# temperature_schedule = lambda k: 50.0 * (1 / 50)**(k/39) if k< 40 else 1.0  # Exponential decay schedule for temperature
+temperature_schedule = lambda k: 50.0  # constant temperature schedule 
 
 # load pretrained score network
 score_nn = ScoreNetwork(input_dim=n+1, out_dim=n, hidden_dim=32, num_blocks=2)
-score_nn.load_state_dict(torch.load(f'network/toy_score_network_timesteps{steps}.pth'))
+score_nn.load_state_dict(torch.load(f'network/toy_score_network_timesteps{steps}_v2.pth'))
 
 # Nonlinear system dynamics
 def g(x):
@@ -142,8 +143,17 @@ def original_pdf(x):
     p2 = torch.exp(-0.5 * ((x - m2) / sigma)**2) / (sigma * torch.sqrt(torch.tensor(2.0) * torch.pi))
     return 0.5 * (p1 + p2)
 
+# def leftshifted_pdf(x):
+#     m1 = 2.5
+#     m2 = -3.5
+#     sigma = 1.0
+#     p1 = torch.exp(-0.5 * ((x - m1) / sigma)**2) / (sigma * torch.sqrt(torch.tensor(2.0) * torch.pi))
+#     p2 = torch.exp(-0.5 * ((x - m2) / sigma)**2) / (sigma * torch.sqrt(torch.tensor(2.0) * torch.pi))
+#     return 0.5 * (p1 + p2)
+
+
 def tilted_pdf(x, temperature):
-    return original_pdf(x)*torch.exp(-(x-3)**2/(2*temperature))
+    return original_pdf(x)*torch.exp(-(x-3.0)**2/(2*temperature))
 
 class UNetFromPhi(nn.Module):
     def __init__(self, phi_net, g_fn, temperature):
@@ -165,7 +175,8 @@ time_grid = torch.arange(0, steps+1) * dt
 mu = torch.zeros(n, requires_grad=True)  # Mean of initial distribution
 Q = torch.eye(n, requires_grad=True)  # Sigma = Q Q^T for initial distribution
 ut = None  # Initialize ut as None, which means no control at the beginning
-
+mu_opt = torch.optim.AdamW([mu], lr=3e-4, weight_decay=1e-4)
+Q_opt = torch.optim.AdamW([Q], lr=3e-4, weight_decay=1e-4)
 
 #### We don't need to retian score because phi PDE still holds for a wrong s(t,x). ####
 for k in range(kf):
@@ -183,29 +194,66 @@ for k in range(kf):
     Y_T = partial_lf(X_T)  # shape (N, n)
     # plt.figure()
     # plt.plot(X_b[:, :1000, 0].detach().numpy(), color='blue', alpha=0.1)
+    # plt.title(f'Backward Rollout of X_t at Iteration {k+1}')
+    # plt.show()
+    # plt.figure()
+    # plt.plot(X_f[:, :1000, 0].detach().numpy(), color='blue', alpha=0.1)
+    # plt.title(f'Forward Rollout of X_t at Iteration {k+1}')
+    # plt.show()
+    # plt.figure()
+    # plt.hist(X_f[-1, :, 0].detach().numpy(), bins=50, color='blue', alpha=0.5, density=True)
+    # plt.plot(torch.linspace(-8, 8, 1000).numpy(), tilted_pdf(torch.linspace(-8, 8, 1000), temperature), label='tilted pdf', color='red')
+    # plt.title(f'Distribution of forward $X_T$ at Iteration {k+1}')
+    # plt.show()
+    # plt.figure()
+    # plt.hist(X_b[-1, :, 0].detach().numpy(), bins=50, color='blue', alpha=0.5, density=True)
+    # plt.plot(torch.linspace(-8, 8, 1000).numpy(), tilted_pdf(torch.linspace(-8, 8, 1000), temperature), label='tilted pdf', color='red')
+    # plt.title(f'Distribution of backward $X_T$ at Iteration {k+1}')
+    # plt.show()
+    # plt.figure()
+    # plt.hist(Y_T[:, 0].detach().numpy(), bins=50, color='blue', alpha=0.5, density=True)
+    # plt.title(f'Distribution of terminal costate $Y_T$ at Iteration {k+1}')
+    # plt.plot(torch.linspace(-11, 5, 1000).numpy(), leftshifted_pdf(torch.linspace(-11, 5, 1000)), label='tilted pdf', color='red')
     # plt.show()
     # if k == 0:
     #     uf = torch.zeros_like(X_f)  # shape (steps+1, N, m)
     # else:
     #     uf = ut(X_f.reshape(-1, n), time_grid.repeat_interleave(N, dim=0).reshape(-1, 1)).reshape(steps+1, N, m).detach()  # shape (steps+1, N, m)
+    
 
     # Train phi network
-    phi_net = ScoreNetwork(input_dim=n+1, out_dim=n, hidden_dim=64, num_blocks=4)
-    optimizer_phi = torch.optim.AdamW(phi_net.parameters(), lr=2e-5, weight_decay=1e-4)
+    if k == 0:
+        phi_net = ScoreNetwork(input_dim=n+1, out_dim=n, hidden_dim=64, num_blocks=4)
+    optimizer_phi = torch.optim.AdamW(phi_net.parameters(), lr=1e-5, weight_decay=1e-4)
     scheduler_phi = torch.optim.lr_scheduler.StepLR(optimizer_phi, step_size=500, gamma=0.9)
 
     for phi_i in range(phi_iter):
         print(f"Total iteration {k+1}/{kf} | Phi training iteration {phi_i+1}/{phi_iter}")
+        phi_net.eval()
         Y_b = time_reversal_bsde(H_x, g, phi_net, T, dt, Y_T, W_b, [score_nn], X_b, nn_num=1)
+        phi_net.train()
+        # plt.figure()
+        # plt.plot(Y_b[:, :1000, 0].detach().numpy(), color='blue', alpha=0.1)
+        # plt.plot(X_b[:, :1000, 0].detach().numpy(), color='red', alpha=0.1)
+        # plt.title(f'Backward Rollout of Y_t with Trained Phi at Iteration {k+1}, Phi Iteration {phi_i+1}')
+        # plt.show()
         # phi_net = ScoreNetwork(input_dim=n+1, out_dim=n, hidden_dim=64, num_blocks=4)
         # optimizer_phi = torch.optim.AdamW(phi_net.parameters(), lr=1e-5, weight_decay=1e-4)
-        # scheduler_phi = torch.optim.lr_scheduler.StepLR(optimizer_phi, step_size=2000, gamma=0.9)
-        phi_loss_history = train_phi_network(phi_net, X_b, Y_b, time_grid, optimizer_phi, scheduler_phi, batch_size=64, iterations=3000)
-
+        # scheduler_phi = torch.optim.lr_scheduler.StepLR(optimizer_phi, step_size=500, gamma=0.9)
+        phi_loss_history = train_phi_network(phi_net, X_b, Y_b, time_grid, optimizer_phi, scheduler_phi, batch_size=64, iterations=1500)
+    
+    torch.save(X_f, f'data/finetune_Xf_timesteps{steps}_optiter{opt_iter}_const_temperature{temperature}_iteration{k+1}in{kf}.pth')
+    torch.save(X_b, f'data/finetune_Xb_timesteps{steps}_optiter{opt_iter}_const_temperature{temperature}_iteration{k+1}in{kf}.pth')
+    torch.save(Y_b, f'data/finetune_Yb_timesteps{steps}_optiter{opt_iter}_const_temperature{temperature}_iteration{k+1}in{kf}.pth')
+    # plt.figure()
+    # plt.plot(Y_b[:, :1000, 0].detach().numpy(), color='blue', alpha=0.1)
+    # plt.plot(X_b[:, :1000, 0].detach().numpy(), color='red', alpha=0.1)
+    # plt.plot(X_f[:, :1000, 0].detach().numpy(), color='green', alpha=0.1)
+    # plt.title(f'Backward Rollout of Y_t with Trained Phi at Iteration {k+1}, Phi Iteration {phi_i+1}')
+    # plt.show()
     
     # Optimize initial distribution parameters and feedback control law
-    mu_opt = torch.optim.AdamW([mu], lr=1e-2, weight_decay=1e-4)
-    Q_opt = torch.optim.AdamW([Q], lr=1e-2, weight_decay=1e-4)
+    
     # if ut is None:
     #     ut = torch.zeros((steps+1, N, m), requires_grad=True)  # shape (steps+1, N, m)
     # ut_opt = torch.optim.AdamW([ut], lr=1e-2, weight_decay=1e-4)
@@ -248,6 +296,7 @@ for k in range(kf):
     # Update ut with the trained phi network
     # ut = UNetFromPhi(phi_net, g).eval()
     X_f = rollout(f, g, T, dt, (torch.randn(N, n) @ Q + mu).detach(), torch.randn(steps + 1, N, m) * torch.sqrt(torch.tensor(dt)), u_t=ut)  # shape (steps+1, N, n)
+    X_b = rollout(special_f, g, T, dt, X_T, W_b.flip(dims=[0]), u_t=ut).detach().flip(dims=[0])
     if k == kf-1:
         plt.figure()
         plt.plot(X_f[:, :1000, 0].detach().numpy(), color='blue', alpha=0.1)
@@ -264,11 +313,10 @@ for k in range(kf):
         print(f"Final mu: {mu.detach().numpy()} | Final Q: {Q.detach().numpy()}")
         print(f"Final mean and std of X_T: {X_f[-1, :, 0].mean().item()} | {X_f[-1, :, 0].std().item()}")
     
-    
-    torch.save(X_f, f'data/finetune_Xf_timesteps{steps}_temperature{temperature}.pth')
+    print(f"Final mean and std of X_T: {X_f[-1, :, 0].mean().item()} | {X_f[-1, :, 0].std().item()}")
 
-torch.save(phi_net.state_dict(), f'network/finetune_phi_network_timesteps{steps}_iteration{kf}_phiiter{phi_iter}.pth')
-torch.save(mu, f'network/finetune_mu_timesteps{steps}_iteration{kf}_phiiter{phi_iter}.pth')
-torch.save(Q, f'network/finetune_Q_timesteps{steps}_iteration{kf}_phiiter{phi_iter}.pth')
+torch.save(phi_net.state_dict(), f'network/finetune_phi_network_timesteps{steps}_iteration{kf}_phiiter{phi_iter}_optiter{opt_iter}_temperature{temperature}.pth')
+torch.save(mu, f'network/finetune_mu_timesteps{steps}_iteration{kf}_phiiter{phi_iter}_optiter{opt_iter}_temperature{temperature}.pth')
+torch.save(Q, f'network/finetune_Q_timesteps{steps}_iteration{kf}_phiiter{phi_iter}_optiter{opt_iter}_temperature{temperature}.pth')
 
-# torch.save(ut, f'network/finetune_ut_timesteps{steps}_iteration{kf}_phiiter{phi_iter}.pth')
+# torch.save(ut, f'network/finetune_ut_timesteps{steps}_iteration{kf}_phiiter{phi_iter}_temperature{temperature}.pth')
